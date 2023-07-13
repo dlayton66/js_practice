899788a754219e83693a7e49ba1d9f7d00e986c8
Drew Layton <dlayton66@gmail.com>
2023-07-13 14:22:25 -0600

    feat: Functions 1

---
+++ lasagna-master.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+/**
+ * Implement the functions needed to solve the exercise here.
+ * Do not forget to export them so they are available for the
+ * tests. Here an example of the syntax as reminder:
+ *
+ * export function yourFunction(...) {
+ *   ...
+ * }
+ */
+
+ export function cookingStatus(remainingTime) {
+  switch(remainingTime) {
+    case 0:
+      return 'Lasagna is done.';
+    case undefined: 
+      return 'You forgot to set the timer.';
+    default:
+      return 'Not done, please wait.';
+  }
+}
+
+export function preparationTime(layers, layerPrepTime = 2) {
+  return layers.length * layerPrepTime;
+}
+
+export function quantities(layers) {
+  let noodles = 0;
+  let sauce = 0;
+  
+  for(let i = 0; i < layers.length; i++) {
+    if (layers[i] === 'noodles') {
+      noodles += 50;
+    } else if (layers[i] === 'sauce') {
+      sauce += 0.2;
+    }
+  }
+
+  return {
+    noodles: noodles,
+    sauce: sauce
+  }
+}
